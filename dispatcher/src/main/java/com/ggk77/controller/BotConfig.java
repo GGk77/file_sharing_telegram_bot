40249bbcf87eec4891bee72f54acbcb559b933bf
ggk77 <kirill.rekuts@yandex.ru>
2023-01-19 21:47:04 +0300

    feat: create and start bot

---
+++ dispatcher/src/main/java/com/ggk77/controller/BotConfig.java
@@ -0,0 +1,13 @@
+package com.ggk77.controller;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+public class BotConfig {
+    @Value("${bot.name}") String botName;
+    @Value("${bot.token}") String token;
+    @Value("${bot.chatId}") String chatId;
+}
